f984f1ca97de82f6f74f4e18fdfe06938b88a02f
Cinccino <Primexzt@gmail.com>
2017-07-05 08:22:55 +0800

    main and loginpage

--- OnlineExamination/urls.py
+++ OnlineExamination/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from . import view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^main/', view.main),
+    url(r'^login/', view.login),
 ]
--- OnlineExamination/view.py
+++ OnlineExamination/view.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def hello(request):
+def main(request):
     # context = {}
     # context['hello'] = 'Hello World!'
     return render(request, 'main.html')
 
+def login(request):
+    return render(request,'login.html')
