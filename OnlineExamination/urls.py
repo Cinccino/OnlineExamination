67c67d37cd4f8f92b6369de369b7573e78ec8bf0
Cinccino <Primexzt@gmail.com>
2017-07-09 14:00:37 +0800

    table frame fin

--- OnlineExamination/urls.py
+++ OnlineExamination/urls.py
@@ -23,8 +23,10 @@ urlpatterns = [
     # url(r'^verify/$', view.verify),
     url(r'^login/$', view.loginpage),
     url(r'^admin_main/$', view.admin_main),
-
+ 
     # url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     # url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'main.html'}),
-    url(r'^asset_show_table/$',view.show_asset_in_table,name='show_asset_in_table'),  # 展示资产信息在bootstrap-table里面
+    url(r'^infoeditor/$', view.table),
+    url(r'^asset_show_table/$',view.show_asset_in_table),  # 
+    url(r'^aothertableview/$',view.aothertableview), 
 ]
--- OnlineExamination/view.py
+++ OnlineExamination/view.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Count,Sum
 import json
 from onlinetest import models
+from django.http import JsonResponse
 
 
 def main(request):
@@ -47,30 +48,46 @@ def admin_main(request):
 #         return render(request,'login.html')                    
 
 
-def get_ram_sum_size(asset_id):
-    '''
-    get the size of RAM and disk in total
-    :param asset_id:  asset's id
-    :return:   the size of RAM in total
-    '''
-    all_ram_slot = models.RAM.objects.filter(asset__id=asset_id)
-    all_disk_slot = models.Disk.objects.filter(asset__id=asset_id)
-    ram=0
-    for slot in all_ram_slot:
-        ram=ram+slot.capacity
+# def get_ram_sum_size(asset_id):
+#     '''
+#     get the size of RAM and disk in total
+#     :param asset_id:  asset's id
+#     :return:   the size of RAM in total
+#     '''
+#     all_ram_slot = models.RAM.objects.filter(asset__id=asset_id)
+#     all_disk_slot = models.Disk.objects.filter(asset__id=asset_id)
+#     ram=0
+#     for slot in all_ram_slot:
+#         ram=ram+slot.capacity
 
-    disk = 0
-    for slot in all_disk_slot:
-        disk = disk+slot.capacity
-    return ram,disk
+#     disk = 0
+#     for slot in all_disk_slot:
+#         disk = disk+slot.capacity
+#     return ram,disk
 
+def table(request):
+    return render(request,'infoeditor.html')
+
+def aothertableview(request):
+    qs = models.Admin.objects.all()  # Use the Pandas Manager
+    df = qs
+    template = 'infoeditor.html'
+    #Format the column headers for the Bootstrap table, they're just a list of field names, 
+    #duplicated and turned into dicts like this: {'field': 'foo', 'title: 'foo'}
+    columns = [{'field': "username", 'title': "username"},{'field': "name", 'title': "name"},{'field': "pwd", 'title': "pwd"},{'field': "mail", 'title': "mail"}]
+    #Write the DataFrame to JSON (as easy as can be)
+    json = df # output just the records (no fieldnames) as a collection of tuples
+    #Proceed to create your context object containing the columns and the data
+    context = {
+                'data': json,
+                'columns': columns
+                }
+    #And render it!
+    # print(context)
+    return render(request, template, context)
 
 def show_asset_in_table(request):
-    '''
-    专门处理在服务器资产列表里面的表格信息的方法
-    :param request: 
-    :return: 
-    '''
+
     if request.method == "GET":
         print(request.GET)
         limit = request.GET.get('limit')   # how many items per page
@@ -78,74 +95,37 @@ def show_asset_in_table(request):
         search = request.GET.get('search')
         sort_column = request.GET.get('sort')   # which column need to sort
         order = request.GET.get('order')      # ascending or descending
-        if search:    #    判断是否有搜索字
-            all_records = models.Asset.objects.filter(id=search,asset_type=search,business_unit=search,idc=search)
+        if search:    #    
+            all_records = models.Admin.objects.filter(usrename=search)
         else:
-            all_records = models.Asset.objects.all()   # must be wirte the line code here
-
-        if sort_column:   # 判断是否有排序需求
-            sort_column = sort_column.replace('asset_', '')    
-            if sort_column in ['id','asset_type','sn','name','management_ip','manufactory','type']:   # 如果排序的列表在这些内容里面
-                if order == 'desc':   # 如果排序是反向
-                    sort_column = '-%s' % (sort_column)
-                all_records = models.Asset.objects.all().order_by(sort_column)
-            elif sort_column in ['salt_minion_id','os_release',]:
-                # server__ 表示asset下的外键关联的表server下面的os_release或者其他的字段进行排序
-                sort_column = "server__%s" % (sort_column)
-                if order == 'desc':
-                    sort_column = '-%s'%(sort_column)
-                all_records = models.Asset.objects.all().order_by(sort_column)
-            elif sort_column in ['cpu_model','cpu_count','cpu_core_count']:
-                sort_column = "cpu__%s" %(sort_column)
-                if order == 'desc':
-                    sort_column = '-%s'%(sort_column)
-                all_records = models.Asset.objects.all().order_by(sort_column)
-            elif sort_column in ['rams_size',]:
-                if order == 'desc':
-                    sort_column = '-rams_size'
-                else:
-                    sort_column = 'rams_size'
-                all_records = models.Asset.objects.all().annotate(rams_size = Sum('ram__capacity')).order_by(sort_column)
-            elif sort_column in ['localdisks_size',]:  # using variable of localdisks_size because there have a annotation below of this line
-                if order == "desc":
-                    sort_column = '-localdisks_size'
-                else:
-                    sort_column = 'localdisks_size'
-                #     annotate 是注释的功能,localdisks_size前端传过来的是这个值，后端也必须这样写，Sum方法是django里面的，不是小写的sum方法，
-                # 两者的区别需要注意，Sum（'disk__capacity‘）表示对disk表下面的capacity进行加法计算，返回一个总值.
-                all_records = models.Asset.objects.all().annotate(localdisks_size=Sum('disk__capacity')).order_by(sort_column)   
-
-            elif sort_column in ['idc',]:
-                sort_column = "idc__%s" % (sort_column)
-                if order == 'desc':
-                    sort_column = '-%s'%(sort_column)
-                all_records = models.Asset.objects.all().order_by(sort_column)
-
-            elif sort_column in ['trade_date','create_date']:
-                if order == 'desc':
-                    sort_column = '-%s'%sort_column
-                all_records = models.Asset.objects.all().order_by(sort_column)
-
+            all_records = models.Admin.objects.all()   # must be wirte the line code here
 
         all_records_count=all_records.count()
 
         if not offset:
             offset = 0
         if not limit:
-            limit = 20    # 默认是每页20行的内容，与前端默认行数一致
-        pageinator = Paginator(all_records, limit)   # 开始做分页
+            limit = 20    
+        # pageinator = Paginator(all_records, limit) 
 
         page = int(int(offset) / int(limit) + 1)    
-        response_data = {'total':all_records_count,'rows':[]}   # 必须带有rows和total这2个key，total表示总页数，rows表示每行的内容
+        response_data = {'total':all_records_count,'rows':[]}   
 
-
-        for asset in pageinator.page(page):    
-            ram_disk = get_ram_sum_size(asset.id)    # 获取磁盘和内存的大小
-            # 下面这些asset_开头的key，都是我们在前端定义好了的，前后端必须一致，前端才能接受到数据并且请求.
+        # response_data['rows']=all_records
+        for asset in all_records:    
+            # ram_disk = get_ram_sum_size(asset.id)    
             response_data['rows'].append({
-                "asset_id": '<a href="/asset/asset_list/%d" target="_blank">%d</a>' %(asset.id,asset.id),   
-                "asset_sn" : asset.sn if asset.sn else "",
+                "username": asset.username,   
+                "name" : asset.name,
+                "pwd":asset.pwd,
+                "mail":asset.mail,
                 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             })
-            
-        return  HttpResponse(json.dumps(response_data))    # 需要json处理下数据格式   
+        # total=all_records_count
+        # rows = all_records.Skip(offset).Take(limit).ToList();
+        # return Json(new { total = total, rows = rows }, JsonRequestBehavior.AllowGet);
+        return JsonResponse(response_data)
+        # return  HttpResponse(json.dumps(response_data))    # json
+
+
+    
